45b63dd21a83ae2aa3abaedf76968385ec8cb65b
Paulo Borges <borgespaulo72@yahoo.com.br>
2025-09-29 18:13:40 -0300

    feat: adicionando background-image

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from "react-router";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import "./Global.css";
 
 function App() {
   return (
--- src/Routes/Home.tsx
+++ src/Routes/Home.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "../Global.css";
 
 type projectsProps = {
   name: string;
